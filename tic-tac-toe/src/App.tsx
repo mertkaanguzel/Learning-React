0a46d8fe199d1c318844c9609780155b8c778fde
rtardo <84642031+rtardo@users.noreply.github.com>
2023-07-07 11:37:23 +0300

    Implementing time travel feature

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-  //console.log("Greeting was rendered at", new Date().toLocaleTimeString());
-
   return (
     <button className='square' onClick={onSquareClick}>
       {value}
@@ -51,14 +49,30 @@ function Board({ isNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [isNext, setIsNext] = useState(true);
   const [history, setHistory] = useState([new Array(9).fill(null)]);
-  const currentSquares= history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const isNext = currentMove % 2 === 0;
+  const currentSquares= history[currentMove];
 
   function handlePLay(nextSquares) {
-    setHistory([...history ,nextSquares]);
-    setIsNext(!isNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1); //currentMove + 1 da olur mu
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, moveIndex) => {
+    const description = moveIndex > 0 ? `Go to move #${moveIndex}` : 'Go to starting point';
+
+    return (
+      <li key={moveIndex}>
+        <button onClick={() => jumpTo(moveIndex)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className='game'>
@@ -66,7 +80,7 @@ export default function Game() {
         <Board isNext={isNext} squares={currentSquares} onPlay={handlePLay} />
       </div>
       <div className='game-info'>
-        <ol>TODO</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
